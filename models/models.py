0c72dce65f907edd94ca14b7b5b653e06337b34e
Bisiach Lucio <34654675+LucioBisiach@users.noreply.github.com>
2022-07-09 12:37:42 -0300

    Update models.py

--- models/models.py
+++ models/models.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-}from odoo import models, fields ,api, _
+from odoo import models, fields ,api, _
 from odoo.exceptions import ValidationError
 from odoo.tools import config
 
